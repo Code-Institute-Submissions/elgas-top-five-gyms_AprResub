26ba8c4597aab18a3dcd4f24fa2220aee80ab7bd
elgas <elgasmaci@gmail.com>
2021-08-30 12:36:02 +0000

    Added Maps Markers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
-// Initialize and add the map
-
-function initMap() {
-    let gothenburg = {lat: 57.708870, lng: 11.974560};
-     // The map, centered at Gothenburg
-    let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: gothenburg,
+ function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: { lat: 57.708870, lng: 11.974560 },
     });
-  // The marker, positioned at Gothenburg
-    let marker = new google.maps.Marker({
-    position: gothenburg,
-    map: map,
+    // Create an array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    const markers = locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      });
+    });
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
 
   }
+  const locations = [
+    { lat: 57.70609, lng: 11.96510},
+    {lat: 57.71309002, lng: 12.0051182},
+    {lat: 57.715135, lng: 11.996846317},
+    {lat: 57.70598, lng: 11.96473326},
+    {lat: 57.7014494, lng: 11.9526556},
+    { lat: 57.68309, lng: 11.93458}
+    
+    
+  ];
+  
+    
+
+
+
+  
+  
