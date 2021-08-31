e8656ef19dbac9bfb5ccd4a763d2dd9454b1a665
elgas <elgasmaci@gmail.com>
2021-08-31 12:48:24 +0000

    Saving Changes for maps.js, index.html

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,10 @@
  function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 10,
+      zoom: 5,
       center: { lat: 57.708870, lng: 11.974560 },
     });
-    // Create an array of alphabetical characters used to label the markers.
+
+   // Create an array of alphabetical characters used to label the markers.
     const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // Add some markers to the map.
     // Note: The code uses the JavaScript Array.prototype.map() method to
@@ -13,21 +14,21 @@
       return new google.maps.Marker({
         position: location,
         label: labels[i % labels.length],
-      });
+      });     
     });
     // Add a marker clusterer to manage the markers.
     new MarkerClusterer(map, markers, {
       imagePath:
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
-
+    }); 
   }
   const locations = [
+    //Gothenburg locations
     { lat: 57.70609, lng: 11.96510},
     {lat: 57.71309002, lng: 12.0051182},
     {lat: 57.715135, lng: 11.996846317},
-    {lat: 57.70598, lng: 11.96473326},
-    {lat: 57.7014494, lng: 11.9526556},
-    {lat: 57.68309, lng: 11.93458}
-        
-  ];
+    {lat: 57.70598, lng: 11.96473326}
+    
+    //Stockholm locations
+    //Malmo locations
+   ];
