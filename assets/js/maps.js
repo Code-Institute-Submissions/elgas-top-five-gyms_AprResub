aefb5ee0c139e1929b03d6aec2c09d31e91bf693
elgas <elgasmaci@gmail.com>
2021-08-31 21:07:33 +0000

    Separated citys on diferent pages for html and js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,12 +4,8 @@
       center: { lat: 57.708870, lng: 11.974560 },
     });
 
-   // Create an array of alphabetical characters used to label the markers.
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
     const markers = locations.map((location, i) => {
       return new google.maps.Marker({
         position: location,
