1ee8d2b9779b71e8e6daacbd26455e84b1359b98
elgas <elgasmaci@gmail.com>
2021-09-01 06:40:20 +0000

    Finished version regarding Stockholm gyms

--- assets/js/maps-stockholm.js
+++ assets/js/maps-stockholm.js
@@ -1,10 +1,10 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 5,
-    center: { lat: 57.708870, lng: 11.974560 },
+    zoom: 11,
+    center: { lat: 59.32793221198714, lng: 18.05421685247 },
   });
 
- const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ const labels = ["NW Odenplan", "NW Drotningsgatan", " Sats SoFo", "Sats Sportpalaset", "Sats Stureplan"];
   // Add some markers to the map.
   const markers = locations.map((location, i) => {
     return new google.maps.Marker({
@@ -18,13 +18,12 @@ function initMap() {
     "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   }); 
 }
+
+//Gym locations in Stockholm
 const locations = [
-  //Gothenburg locations
-  { lat: 57.70609, lng: 11.96510},
-  {lat: 57.71309002, lng: 12.0051182},
-  {lat: 57.715135, lng: 11.996846317},
-  {lat: 57.70598, lng: 11.96473326}
-  
-  //Stockholm locations
-  //Malmo locations
+  {lat: 59.34088161048154, lng: 18.048890713844774},
+  {lat: 59.33168062021941, lng: 18.063725969664926},
+  {lat: 59.313823645251574, lng: 18.075732354320607},
+  {lat: 59.33426791757178, lng: 18.075417037126766},
+  {lat: 59.33664540102926, lng: 18.03444322363454}
  ];
