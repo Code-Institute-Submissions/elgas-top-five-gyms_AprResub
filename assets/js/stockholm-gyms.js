1dee6bf3fb28e0e8cb9657ae7f8dc8f60b4082d7
elgas <elgasmaci@gmail.com>
2022-04-24 07:00:25 +0000

    Added all gyms to stockholm-gym.js and tested links

--- assets/js/stockholm-gyms.js
+++ assets/js/stockholm-gyms.js
@@ -10,6 +10,14 @@ const listOfGymsStockholm = [
     {
         name:'SATS Stureplan', 
         href:'https://www.sats.se/gym/stockholm/sats-stureplan/'
+    },
+    {
+        name:'Nordic Wellness Odenplan', 
+        href:'https://nordicwellness.se/vara-klubbar/stockholm/stockholm-odenplan/'
+    },
+    {
+        name:'Nordic Wellness Drottninggatan', 
+        href:'https://nordicwellness.se/vara-klubbar/stockholm/stockholm-drottninggatan/'
     }
 ];
 
@@ -18,36 +26,10 @@ let text = "";
 // Iterate through the gyms
 
 for (gyms of listOfGymsStockholm){
-let newbuttons = `
-    <button type="button" class="btn btn-light"><a href="${listOfGymsStockholm.href}" target="_blank">${listOfGymsStockholm.name}</a></button>
+let addButtons = `
+    <button type="button" class="btn btn-light"><a href="${gyms.href}" target="_blank">${gyms.name}</a></button>
     `;
-    text += newbuttons;
+    text += addButtons;
 }
 
 document.getElementById("gym").innerHTML = text;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let text = "";
-
-//for (let x in listOfGymsStockholm) {
-//  text += [x] + "<br>";
-//}
-
-//document.getElementById("gym").innerHTML = text;
